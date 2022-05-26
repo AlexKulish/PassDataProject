d1e4cd790059dea8d16d5ddc1b687fe1477cadc8
Alexey Kulish <alexkulish1993@gmail.com>
2022-05-26 12:31:37 +0300

    setup ui elements and constrains add secondVC

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Alex Kulish on 26.05.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+    
+}
